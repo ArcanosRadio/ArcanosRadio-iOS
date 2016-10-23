0ab960066635400736e6980651930a7fafc72108
Luiz Barbosa <luiz.barbosa@here.com>
2016-10-23 02:30:27 +0200

    Acknowledgements and licenses wip

---
+++ ArcanosRadio/Acknowledgements/licenses/genLicense.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+my $plistout =  "../Acknowledgements.plist";
+
+system("rm -f $plistout");
+
+open(my $plistfh, '>', $plistout) or die $!;
+
+print $plistfh <<'EOD';
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+        <key>StringsTable</key>
+        <string>Acknowledgements</string>
+        <key>PreferenceSpecifiers</key>
+        <array>
+EOD
+for my $i (sort glob("*.license"))
+{
+    my $value=`cat $i`;
+    $value =~ s/\r//g;
+    $value =~ s/\n/\r/g;
+    $value =~ s/[ \t]+\r/\r/g;
+    $value =~ s/\"/\'/g;
+    my $key=$i;
+    $key =~ s/\.license$//;
+
+    my $cnt = 1;
+    my $keynum = $key;
+    for my $str (split /\r\r/, $value)
+    {
+        print $plistfh <<"EOD";
+                <dict>
+                        <key>Type</key>
+                        <string>PSGroupSpecifier</string>
+                        <key>Title</key>
+                        <string>$keynum</string>
+                </dict>
+EOD
+
+        $keynum = $key.(++$cnt);
+    }
+}
+
+print $plistfh <<'EOD';
+        </array>
+</dict>
+</plist>
+EOD
+
+close($plistfh);
--- ArcanosRadio/Controller/AWRAboutController.m
+++ ArcanosRadio/Controller/AWRAboutController.m
@@ -1,9 +1,11 @@
 #import "AWRAboutController.h"
 #import "AWRAboutView.h"
 
-@interface AWRAboutController () <AWRAboutViewDelegate>
+@interface AWRAboutController () <AWRAboutViewDelegate, UITableViewDelegate, UITableViewDataSource>
 
 @property(readonly, nonatomic) AWRAboutView *aboutView;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (strong, nonatomic) NSArray *content;
 
 @end
 
@@ -22,6 +24,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"Cell"];
     self.aboutView.delegate = self;
 }
 
@@ -29,6 +32,35 @@
     if (!self.delegate) return;
 
     [self.delegate userDidCloseAbout];
+}
+
+-(NSArray *)content {
+    if (!_content) {
+        NSString *acknowledgementsPlist = [[NSBundle mainBundle]
+                                           pathForResource:@"Acknowledgements"
+                                           ofType:@"plist"];
+        NSDictionary *root = [[NSDictionary alloc] initWithContentsOfFile:acknowledgementsPlist];
+        _content = [root objectForKey:@"PreferenceSpecifiers"];
+    }
+    return _content;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.content count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *CellIdentifier = @"Cell";
+
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+
+    cell.textLabel.text = [[self.content objectAtIndex:indexPath.row] valueForKey:@"Title"];
+    //    cell.detailTextLabel.text = [[self.content objectAtIndex:indexPath.row] valueForKey:@"state"];
+    return cell;
 }
 
 @end
