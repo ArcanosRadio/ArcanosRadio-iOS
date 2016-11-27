86caa863df6d352b89e8e70fd00f88694efa2a61
Luiz Barbosa <luiz.barbosa@here.com>
2016-11-27 01:51:07 +0100

    Array funcional extension

---
+++ ArcanosRadio/BaseLibrary/NSArray+Functional.m
@@ -0,0 +1,15 @@
+#import "NSArray+Functional.h"
+
+@implementation NSArray (Functional)
+
+- (instancetype (^) (void (^)(id)))each {
+    __weak typeof(self) weakSelf = self;
+    return ^id(void (^block)(id)) {
+        for (id item in weakSelf) {
+            block(item);
+        }
+        return weakSelf;
+    };
+}
+
+@end
