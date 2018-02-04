de4ee9d1e1700c0a272a63c4baab202db44bf063
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2018-02-04 16:34:26 +0100

    New project in Swift

---
+++ RadioCore/RadioCore.h
@@ -0,0 +1,18 @@
+//
+//  RadioCore.h
+//  RadioCore
+//
+//  Created by Luiz Rodrigo Martins Barbosa on 04.02.18.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RadioCore.
+FOUNDATION_EXPORT double RadioCoreVersionNumber;
+
+//! Project version string for RadioCore.
+FOUNDATION_EXPORT const unsigned char RadioCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RadioCore/PublicHeader.h>
+
+
